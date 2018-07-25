9b3d077e52a56ed6c77465ae15b6c1b6e7e7984f
megharana <megharanait95@gmail.com>
2018-07-25 23:39:20 +0530

    Avatar & table included

--- app.js
+++ app.js
@@ -3,39 +3,16 @@ new Vue({
   el: '#app',
   data () {
     return {
-      info: null
+      users:[]
     }
   },
   mounted () {
     axios
       .get("http://127.0.0.1:8000/user/")
-      .then(response => (this.info = response.data))
+      .then(response => (this.users = response.data));
+     
 
-  }
+  },
+  
 })
 
-//   http:{
-//     root:'http://127.0.0.1:8000/',
-//     headers:{
-//       Authorization:'',
-//     }
-//   },
-//   methods:{
-//      changeTitle:function(){
-//       this.$http.get('user/').then(function(data){
-//       console.log(data);
-      
-//     })
-//       console.log("Megha")
-//     }
-//   }
-
-  
-//   created(){
-//     this.$http.get('http://127.0.0.1:8000/user/').then(function(data){
-//       console.log(data);
-//     })
-//   }
-
-  
-// })
