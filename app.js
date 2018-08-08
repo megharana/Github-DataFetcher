33751a1c6686e3ed7b40639a31ff1c745a323e09
megharana <megharanait95@gmail.com>
2018-08-09 00:22:10 +0530

    User Searched, list got populated

--- app.js
+++ app.js
@@ -3,16 +3,33 @@ new Vue({
   el: '#app',
   data () {
     return {
-      users:[]
+      users:[],
+      userName:null,
+      usersList:[],
     }
   },
   mounted () {
     axios
       .get("http://127.0.0.1:8000/user/")
       .then(response => (this.users = response.data));
-     
-
   },
-  
+  methods:{
+    addUser:function(){
+      userName=this.userName;
+      console.log(this.userName);
+      axios.post("http://127.0.0.1:8000/users/name/",this.userName);
+      // axios
+      //   .get("http://127.0.0.1:8000/users/list/").then(response => (console.log(response.data)));
+      axios.get("http://127.0.0.1:8000/users/name/").then(response =>   {
+            this.usersList = response.data
+          })
+      console.log(this.usersList);
+        }
+
+    },
+
+      
+          
 })
+
 
